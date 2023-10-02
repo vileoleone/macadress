43d4c7907e3e14d3a5a25ca237058f8a77a66928
vileoleone <vileoleone@gmail.com>
2023-10-02 10:33:05 -0400

    getLinuxParams

--- getLinuxAddress.js
+++ getLinuxAddress.js
@@ -4,19 +4,22 @@ const util = require('node:util')
 const exec = util.promisify(cp.exec)
 
 const getLinuxParams = async () => {
-  const parsedArray = []
+  const parsedArray = {}
   const interfaceNames = await getLinuxInterfaces()
-    .then(interfaces => interfaces)
-    .catch(err => console.log(err))
+    .then((interfaces) => interfaces)
+    .catch((err) => console.log(err))
+
+  const interfaceIpv4 = await getLinuxInet()
+    .then((ips) => ips)
+    .catch((err) => console.log(err))
 
   for (const name of interfaceNames) {
-    const interfaceParams = await getLinuxSingleInterface(name)
-      .then(params => params)
-      .catch(err => console.log(err))
+    const interfaceParams = await getLinuxAddress(name)
+      .then((params) => params)
+      .catch((err) => console.log(err))
 
-    parsedArray.push({ [name]: { macAddress: interfaceParams } })
+    parsedArray[name] = { macAddress: interfaceParams, ipv4: interfaceIpv4[name] }
   }
-  console.log({ parsedArray })
   return parsedArray
 }
 
@@ -24,9 +27,7 @@ const getLinuxInterfaces = async () => {
   try {
     const { stdout } = await exec('/bin/ls /sys/class/net ')
 
-    const interfaces = stdout
-      .split('\n')
-      .filter((iFace) => iFace !== '')
+    const interfaces = stdout.split('\n').filter((iFace) => iFace !== '')
 
     return interfaces
   } catch (err) {
@@ -35,15 +36,44 @@ const getLinuxInterfaces = async () => {
   }
 }
 
-getLinuxInterfaces()
-
-const getLinuxSingleInterface = async (iFace) => {
+const getLinuxAddress = async (iFace) => {
   try {
-    const { stdout } = await exec(`/bin/cat /sys/class/net${iFace}/address`)
+    const { stdout } = await exec(`/bin/cat /sys/class/net/${iFace}/address`)
 
-    return stdout
+    return stdout.trim()
   } catch (err) {
     console.error('Error:', err)
     throw err
   }
+}
+
+const getLinuxInet = async () => {
+  try {
+    const { stdout } = await exec('ip addr show | grep inet | grep -v inet6')
+
+    const parsedResponse = LinuxParser(stdout)
+
+    return parsedResponse
+  } catch (err) {
+    console.error('Error:', err)
+    throw err
+  }
+}
+
+const LinuxParser = (str) => {
+  const formatReturn = {}
+  const lines = str
+    .split(/\n\t|\n/)
+    .map((str) => str.replace("'", '').trim())
+    .filter((str) => str !== '')
+
+  lines.forEach((ipString) => {
+    const ipArray = ipString.split(' ')
+    const lastItem = ipArray.length - 1
+    const ipv4 = ipArray[1]
+    const iFaceName = ipArray[lastItem]
+    formatReturn[iFaceName] = ipv4
+  })
+
+  return formatReturn
 }
